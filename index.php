f6d4ba091b2b6450e6f68cfa4c696bc438eeaffb
captain-milous <milda.tesar3@gmail.com>
2024-01-05 20:14:44 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Vanocni Projekt</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    
+    <div>
+        <section class="form signup">
+            <header>Realtime chat App</header>
+            <form action="#">
+                <div class="error-txt">This is an error message!</div>
+                <div class="name-details">
+                    <div class="field">
+                        <label>Jméno</label>
+                        <input type="text" placeholder="Pepa">
+                    </div>
+                    <div class="field">
+                        <label>Přijmení</label>
+                        <input type="text" placeholder="Novák">
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Email</label>
+                    <input type="email" placeholder="pepa.novak@email.cz">
+                </div>
+                <div class="field">
+                    <label>Heslo</label>
+                    <input type="password" placeholder="Heslo">
+                </div>
+                <div class="field">
+                    <label>Profilový obrázek</label>
+                    <input type="file">
+                </div>
+                <div class="field">
+                    <input type="submit" placeholder="Zaregistrovat se">
+                </div>
+            </form>
+            <div class="link">Už jste zaregistrován? <a href="#">Přihlásit se</a></div>
+        </section>
+    </div>
+</body>
+</html>

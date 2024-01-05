a916004deb6b0436c1a40d657b060d981cb1f727
captain-milous <milda.tesar3@gmail.com>
2024-01-05 21:58:40 +0100

    users

--- users.php
+++ users.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Vanocni Projekt</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/users.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 </head>
@@ -27,6 +27,28 @@
                 <span>Zadejte jméno uživatele</span>
                 <input type="text" placeholder="Hledej jméno">
                 <button><i class="fas fa-search"></i></button>
+            </div>
+            <div class="users-list">
+                <a href="#">
+                    <div class="content">
+                        <img src="img/unknown.png" alt="">
+                        <div class="details">
+                            <span>Miloš Tesař</span>
+                            <p>Test zpráva</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+                <a href="#">
+                    <div class="content">
+                        <img src="img/unknown.png" alt="">
+                        <div class="details">
+                            <span>Šolim Řaset</span>
+                            <p>Test zpráva</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
             </div>
         </section>
     </div>

6569dfdeb9360f2d5f2fabaae9403bb39a318ed9
captain-milous <milda.tesar3@gmail.com>
2024-01-05 21:28:04 +0100

    users

---
+++ users.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Vanocni Projekt</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    
+    <div class="wrapper">
+        <section class="users">
+            <header>
+                <div class="content">
+                    <img src="img/unknown.png" alt="Profilový Obrázek xd" />
+                    <div class="details">
+                        <span>Miloš Tesař</span>
+                        <p>Aktivní</p>
+                    </div>
+                </div>
+                <a class="logout">Odhlásit se</a>
+            </header>
+            <div class="search">
+                <span>Zadejte jméno uživatele</span>
+                <input type="text" placeholder="Hledej jméno">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+        </section>
+    </div>
+</body>
+</html>

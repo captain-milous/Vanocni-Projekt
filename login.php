0e473187e647f8d2c1e2802a032d40a2b5c1bc3d
captain-milous <milda.tesar3@gmail.com>
2024-01-05 21:46:30 +0100

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Vanocni Projekt</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/signup.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 </head>
 <body>

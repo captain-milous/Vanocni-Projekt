519c00e589091b2bce526db5f1fb0f3d721973f8
captain-milous <milda.tesar3@gmail.com>
2024-01-07 11:55:26 +0100

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,3 +1,10 @@
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,28 +21,28 @@
     <div class="wrapper">
         <section class="chat-area">
             <header>
+                <?php 
+                $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+                if(mysqli_num_rows($sql) > 0){
+                    $row = mysqli_fetch_assoc($sql);
+                }else{
+                    header("location: users.php");
+                }
+                ?>
                 <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-                <img src="img/unknown.png" alt="Profilový Obrázek xd" />
+                <img src="php/images/<?php echo $row['img']; ?>" alt="">
                 <div class="details">
-                    <span>Miloš Tesař</span>
-                    <p>Aktivní</p>
+                <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+                <p><?php echo $row['status']; ?></p>
                 </div>
             </header>
             <div class="chat-box">
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Zpráva</p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="img/unknown.png" alt="Profilovka">
-                    <div class="details">
-                        <p>Zpráva</p>
-                    </div>
-                </div>
+                
             </div>
             <form action="#" class="typing-area">
-                <input type="text" placeholder="Zpráva..">
+                <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+                <input type="text" name="message" class="input-field" placeholder="Zpráva..." autocomplete="off">
                 <button><i class="fab fa-telegram-plane"></i></button>
             </form>
         </section>

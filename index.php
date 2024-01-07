38e213c1a6d17f5f517edc340e1b5becc15a416e
captain-milous <milda.tesar3@gmail.com>
2024-01-07 11:55:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +16,7 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     
     <script src="scripts/pass-show-hide.js" defer></script>
+    <script src="scripts/signup.js" defer></script>
 
 </head>
 <body>
@@ -17,33 +24,33 @@
     <div class="wrapper">
         <section class="form signup">
             <header>Realtime chat App</header>
-            <form action="#">
+            <form id="form" action="#" enctype="multipart/form-data">
                 <div class="error-txt">This is an error message!</div>
                 <div class="name-details">
                     <div class="field input">
                         <label>Jméno</label>
-                        <input type="text" placeholder="Pepa">
+                        <input type="text" name="fname" placeholder="Pepa" required>
                     </div>
                     <div class="field input">
                         <label>Přijmení</label>
-                        <input type="text" placeholder="Novák">
+                        <input type="text" name="lname" placeholder="Novák" required>
                     </div>
                 </div>
                 <div class="field input">
                     <label>Email</label>
-                    <input type="email" placeholder="pepa.novak@email.cz">
+                    <input type="email" name="email" placeholder="pepa.novak@email.cz" required>
                 </div>
                 <div class="field input">
                     <label>Heslo</label>
-                    <input id="password" type="password" placeholder="h0vN0-k13sL0?">
+                    <input id="password" name="password" type="password" placeholder="h0vN0-k13sL0?" required>
                     <i class="fas fa-eye" onclick="myFunction()"></i>
                 </div>
-                <div class="field image">
+                <div class="field image" name="image" required>
                     <label>Profilový obrázek</label>
                     <input type="file">
                 </div>
                 <div class="field button">
-                    <input type="submit" placeholder="Zaregistrovat se">
+                    <input type="submit" placeholder="Zaregistrovat se" onclick="registration()">
                 </div>
             </form>
             <div class="link">Už jste zaregistrován? <a href="login.php">Přihlásit se</a></div>

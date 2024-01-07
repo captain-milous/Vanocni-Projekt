cda4dfaeb8f3880d4b7fe537e7797a966df4e4a9
captain-milous <milda.tesar3@gmail.com>
2024-01-07 11:55:23 +0100

    Update users.php

--- users.php
+++ users.php
@@ -1,3 +1,10 @@
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,42 +23,28 @@
     
     <div class="wrapper">
         <section class="users">
-            <header>
-                <div class="content">
-                    <img src="img/unknown.png" alt="Profilový Obrázek xd" />
-                    <div class="details">
-                        <span>Miloš Tesař</span>
-                        <p>Aktivní</p>
-                    </div>
-                </div>
-                <a href="login.php" class="logout">Odhlásit se</a>
-            </header>
+        <header>
+        <div class="content">
+          <?php 
+            $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+            if(mysqli_num_rows($sql) > 0){
+              $row = mysqli_fetch_assoc($sql);
+            }
+          ?>
+          <img src="php/images/<?php echo $row['img']; ?>" alt="">
+          <div class="details">
+            <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+            <p><?php echo $row['status']; ?></p>
+          </div>
+        </div>
+        <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Odhlásit se</a>
+      </header>
             <div class="search">
                 <span>Zadejte jméno uživatele</span>
                 <input id="searchBar" type="text" placeholder="Hledej jméno">
                 <button onclick="searchFunction()"><i class="fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="img/unknown.png" alt="">
-                        <div class="details">
-                            <span>Miloš Tesař</span>
-                            <p>Test zpráva</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="img/unknown.png" alt="">
-                        <div class="details">
-                            <span>Šolim Řaset</span>
-                            <p>Test zpráva</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
                 
             </div>
         </section>
